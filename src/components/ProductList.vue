be916ca901fb10da4fa7c47c233b4174bf34ebfb
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-05-11 15:22:29 +0900

    ProductList 스타일 추가

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,39 +1,39 @@
 <template>
-  <div class="product-list-wrap">
-    <h1 class="visually-hidden">상품리스트</h1>
-    <ul class="product-list">
-      <li class="product-item">
-        <a href="">
-          <div class="product-item-img">
-            <img src="../assets/product-img1.jpg" alt="맥북" />
-          </div>
-          <div class="product-info">
-            <p class="product-title">맥북</p>
-            <ul>
-              <li>중랑구 신내2동</li>
-              <li>32분전</li>
-            </ul>
-            <strong>10,000원</strong>
-          </div>
-        </a>
-      </li>
-      <li class="product-item">
-        <a href="">
-          <div class="product-item-img">
-            <img src="../assets/product-img2.jpg" alt="아이패드" />
-          </div>
-          <div class="product-info">
-            <p class="product-title">아이패드</p>
-            <ul>
-              <li>중랑구 신내2동</li>
-              <li>50분전</li>
-            </ul>
-            <strong>10,000원</strong>
-          </div>
-        </a>
-      </li>
-    </ul>
-  </div>
+  <h1 class="visually-hidden">상품리스트</h1>
+  <ul class="product-list">
+    <li class="product-item">
+      <a href="#" class="product-item-link">
+        <div class="product-item-img">
+          <img src="../assets/product-img1.jpg" alt="맥북" />
+        </div>
+        <div class="product-item-info">
+          <p class="product-item-info-title">
+            누구나 멋있게 사용할수 있는 아주 비싸고 멋진 맥북입니다. 이거 득템임
+          </p>
+          <ul>
+            <li>중랑구 신내2동</li>
+            <li>32분전</li>
+          </ul>
+          <strong>10,000원</strong>
+        </div>
+      </a>
+    </li>
+    <li class="product-item">
+      <a href="#" class="product-item-link">
+        <div class="product-item-img">
+          <img src="../assets/product-img2.jpg" alt="아이패드" />
+        </div>
+        <div class="product-item-info">
+          <p class="product-item-info-title">아이패드</p>
+          <ul>
+            <li>중랑구 신내2동</li>
+            <li>50분전</li>
+          </ul>
+          <strong>10,000원</strong>
+        </div>
+      </a>
+    </li>
+  </ul>
 </template>
 
 <script>
