57f047a23919e50862d2aa867942d6cc7fb0c817
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-05-11 14:35:55 +0900

    ProductList 컴포넌트 분리

--- src/App.vue
+++ src/App.vue
@@ -6,43 +6,7 @@
 
     <FloatButton />
 
-    <!--상품리스트-->
-    <!-- <div class="product-list-wrap">
-      <h1 class="visually-hidden">상품리스트</h1>
-      <ul class="product-list">
-        <li class="product-item">
-          <a href="">
-            <div class="product-item-img">
-              <img src="./assets/product-img1.jpg" alt="맥북" />
-            </div>
-            <div class="product-info">
-              <p class="product-title">맥북</p>
-              <ul>
-                <li>중랑구 신내2동</li>
-                <li>32분전</li>
-              </ul>
-              <strong>10,000원</strong>
-            </div>
-          </a>
-        </li>
-        <li class="product-item">
-          <a href="">
-            <div class="product-item-img">
-              <img src="./assets/product-img2.jpg" alt="아이패드" />
-            </div>
-            <div class="product-info">
-              <p class="product-title">아이패드</p>
-              <ul>
-                <li>중랑구 신내2동</li>
-                <li>50분전</li>
-              </ul>
-              <strong>10,000원</strong>
-            </div>
-          </a>
-        </li>
-      </ul>
-    </div> -->
-    <!--//상품리스트-->
+    <ProductList />
   </div>
 </template>
 
@@ -50,6 +14,7 @@
 import GlobalHeader from "./components/GlobalHeader.vue";
 import GlobalTabBar from "./components/GlobalTabBar.vue";
 import FloatButton from "./components/FloatButton.vue";
+import ProductList from "./components/ProductList";
 
 export default {
   name: "App",
@@ -57,6 +22,7 @@ export default {
     GlobalHeader,
     GlobalTabBar,
     FloatButton,
+    ProductList,
   },
 };
 </script>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="product-list-wrap">
+    <h1 class="visually-hidden">상품리스트</h1>
+    <ul class="product-list">
+      <li class="product-item">
+        <a href="">
+          <div class="product-item-img">
+            <img src="../assets/product-img1.jpg" alt="맥북" />
+          </div>
+          <div class="product-info">
+            <p class="product-title">맥북</p>
+            <ul>
+              <li>중랑구 신내2동</li>
+              <li>32분전</li>
+            </ul>
+            <strong>10,000원</strong>
+          </div>
+        </a>
+      </li>
+      <li class="product-item">
+        <a href="">
+          <div class="product-item-img">
+            <img src="../assets/product-img2.jpg" alt="아이패드" />
+          </div>
+          <div class="product-info">
+            <p class="product-title">아이패드</p>
+            <ul>
+              <li>중랑구 신내2동</li>
+              <li>50분전</li>
+            </ul>
+            <strong>10,000원</strong>
+          </div>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductList",
+};
+</script>
