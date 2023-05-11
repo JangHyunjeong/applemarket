7fa8cf83e81ca7cd5e5d65c37595f2939c5622bb
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-05-11 17:38:23 +0900

    ProductInfo 컴포넌트 분리 및 스타일 추가

---
+++ src/components/ProductInfo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="product-info">
+    <h3 class="product-info-title">
+      누구나 멋있게 사용할수 있는 아주 비싸고 멋진 맥북입니다. 이거 득템임
+    </h3>
+    <ul class="product-info-cate">
+      <li class="cate">전자기기</li>
+      <li>32분전</li>
+    </ul>
+
+    <div class="product-content">
+      <p>
+        1년 밖에 사용하지 않은 m2 맥북입니다.<br />
+        민트급 상품이고요, 컴퓨터로 작업을 잘 하지 않아 보냅니다
+      </p>
+    </div>
+
+    <ul class="product-info-popular">
+      <li>채팅 2</li>
+      <li>관심 13</li>
+      <li>조회 421</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductInfo",
+};
+</script>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -8,28 +8,7 @@
 
     <UserInfoVue />
 
-    <div class="product-info">
-      <h3 class="product-info-title">
-        누구나 멋있게 사용할수 있는 아주 비싸고 멋진 맥북입니다. 이거 득템임
-      </h3>
-      <ul class="product-info-cate">
-        <li class="cate">전자기기</li>
-        <li>32분전</li>
-      </ul>
-
-      <div class="product-content">
-        <p>
-          1년 밖에 사용하지 않은 m2 맥북입니다.<br />
-          민트급 상품이고요, 컴퓨터로 작업을 잘 하지 않아 보냅니다
-        </p>
-      </div>
-
-      <ul class="product-info-popular">
-        <li>채팅 2</li>
-        <li>관심 13</li>
-        <li>조회 421</li>
-      </ul>
-    </div>
+    <ProductInfo />
 
     <div class="product-cta">
       <button aria-label="위시리스트에 추가">
@@ -44,11 +23,13 @@
 
 <script>
 import UserInfoVue from "@/components/UserInfo.vue";
+import ProductInfo from "@/components/ProductInfo.vue";
 
 export default {
   name: "ProductView",
   components: {
     UserInfoVue,
+    ProductInfo,
   },
 };
 </script>
