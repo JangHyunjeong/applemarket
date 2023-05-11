a857ecb0bc60241a4fbbf5a8fc17211ec6392422
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-05-11 21:07:47 +0900

    InputGroup 컴포넌트 분리 및 스타일링

---
+++ src/components/InputGroup.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="input-group">
+    <input class="" type="text" placeholder="제목" />
+    <input type="number" placeholder="가격" />
+    <textarea
+      name=""
+      id=""
+      cols="30"
+      rows="10"
+      placeholder="게시글 내용을 작성해주세요(가품 및 판매 금지 물품은 게시가 제한될 수 있어요)"
+    ></textarea>
+  </div>
+</template>
+<script>
+export default {
+  name: "InputGroup",
+};
+</script>
