c09678d6408df8abcdcf2e5103953f582008fbbe
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-05-11 18:13:10 +0900

    ProductCta 스타일링

---
+++ src/components/ProductCta.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="product-cta">
+    <div class="left">
+      <button class="button-wish" aria-label="위시리스트에 추가">
+        <i class="xi-heart-o" aria-hidden="true"></i>
+      </button>
+      <div class="divider" aria-hidden="true"></div>
+      <strong class="price">10,000원</strong>
+    </div>
+
+    <button
+      class="button-chat button-h38 button-primary"
+      aria-label="채팅하기 버튼"
+    >
+      채팅하기
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductCta",
+};
+</script>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -10,26 +10,21 @@
 
     <ProductInfo />
 
-    <div class="product-cta">
-      <button aria-label="위시리스트에 추가">
-        <i class="xi-heart-o" aria-hidden="true"></i>
-      </button>
-      <strong>10,000dnjs</strong>
-
-      <button aria-label="채팅하기 버튼">채팅하기</button>
-    </div>
+    <ProductCta />
   </div>
 </template>
 
 <script>
 import UserInfoVue from "@/components/UserInfo.vue";
 import ProductInfo from "@/components/ProductInfo.vue";
+import ProductCta from "@/components/ProductCta.vue";
 
 export default {
   name: "ProductView",
   components: {
     UserInfoVue,
     ProductInfo,
+    ProductCta,
   },
 };
 </script>
